841731b0ab98d5b7c2287854114a37a04770bfbe
Miki Yutani <mkyutani@gmail.com>
2021-08-31 15:54:34 +0900

    Catch slack_sdk exception

--- app/ifslack.py
+++ app/ifslack.py
@@ -3,6 +3,7 @@
 import os
 
 from slack_sdk import WebClient
+from slack_sdk.errors import SlackClientError
 from ifprint import PrintInterface
 
 class SlackInterface(PrintInterface):
@@ -12,4 +13,7 @@ class SlackInterface(PrintInterface):
         self.client = WebClient(token=token)
 
     def print(self, title, message, channel, hash=None):
-        self.client.chat_postMessage(channel=channel, text=' '.join([message]))
+        try:
+            self.client.chat_postMessage(channel=channel, text=' '.join([message]))
+        except SlackClientError as e:
+            print(e)
